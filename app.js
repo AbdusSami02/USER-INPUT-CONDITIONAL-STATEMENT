808c304b2c5407aa0a8f72d103613a71031a1a3b
Abdus Sami <abdussamijawed@gmail.com>
2024-07-20 01:36:36 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,119 @@
+document.write('<h1>Task1 </h1>')
+var cityInput = prompt("Enter your city name");
+if (cityInput === "Karachi") {
+  document.write("Welcome to city of Lights");
+}
+//task2
+document.write('<h1>Task2 </h1>')
+var gender = prompt("enter your gender")
+if (gender === "male") {
+    document.write("Good morning sir")
+}
+    if (gender === "female") {
+        document.write("Good mornning MaM")
+    }
+ else {
+    document.write("Your gender is not right")
+}
+//task3 
+document.write('<h1>Task3 </h1>')
+var light = prompt("Enter traffic light signal")
+if (light === "red") {
+    document.write("Must Stop")
+}
+if (light === "yellow") {
+    document.write("Ready to move")
+}
+if (light === green) {
+    document.write("Move now")
+}
+else {
+    document.write("Not a traffic light signal")    
+}
+//task 4
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true) {
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// } 
+
+// // a part condition is true
+// var a = 4;
+// if (++a === 5) {
+//   alert("given condition for variable a is true");
+// }
+
+// // b part condition no 2 & condition no 4 is true
+// var b = 82;
+// if (b++ === 83) {
+//   alert("given condition for variable b is true");
+// }
+
+// // c part
+// var c = 12;
+// if (c++ === 13) {
+//   alert("condition 1 is true");
+// }
+// if (c === 13) {
+//   alert("condition 2 is true");
+// }
+// if (++c < 14) {
+//   alert("condition 3 is true");
+// }
+// if (c === 14) {
+//   alert("condition 4 is true");
+// }
+
+// // d part condition is true
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//   alert("The cost equals");
+// }
+
+// //e part true condition is true
+// if (true) {
+//   alert("True");
+// }
+// if (false) {
+//   alert("False");
+// }
+
+// // f part condition is true
+// if ("car" < "cat") {
+//   alert("car is smaller than cat");
+// }
